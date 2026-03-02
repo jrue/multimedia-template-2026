c0c1cd5647f0dbdca7da9d05665d1bad8d0d6d71
mariepastora <mpp2134@columbia.edu>
2026-03-01 21:04:25 -0500

    pushing datawrapper componen and readme

---
+++ src/lib/components/DatawrapperEmbed.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	export let id: string | undefined;;
+
+	let containerId = `datawrapper-vis-${id}`;
+
+  // replicates datawrapper embed script code!
+	onMount(() => {
+		const script = document.createElement('script');
+		script.src = `https://datawrapper.dwcdn.net/${id}/embed.js`;
+		script.defer = true;
+		script.setAttribute('data-target', `#${containerId}`);
+		document.body.appendChild(script);
+	});
+</script>
+
+<div id={containerId}></div>
